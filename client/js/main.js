1f7ff66fe8b4384472012ab31ed7e80714b4f18e
Sinh <sinh9x0@gmail.com>
2016-02-10 23:30:04 -0800

    installed xlsx to parse excel, returnedjson to client

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,10 @@
 app.controller('mainController', function(fileFactory, $scope) {
 	var _this = this;
     $scope.uploadFile = function(event){
-    	console.log(event.target.files[0])
         fileFactory.sendFile(event.target.files[0], function(data){
-        	_this.data = data;
+        	console.log(data.content[data.sheets[0]]);
+        	_this.data = data.content;
+        	_this.sheets = data.sheets;
         })
     };
 
@@ -15,7 +16,6 @@ app.factory('fileFactory', function($http){
 	factory.sendFile = function(file, callback){
 		var fd = new FormData();
         fd.append('file', file);
-        console.log(fd);
 		$http.post('/sendFile/', fd, {
 				transformRequest: angular.identity,
             	headers: {'Content-Type': undefined}
--- server/controllers/files.js
+++ server/controllers/files.js
@@ -1,20 +1,29 @@
 var mongoose = require('mongoose');
 var formidable = require('formidable');
 var util = require('util');
+var excel = require('xlsx');
+var fs = require('fs')
 
 
 module.exports = (function() {
 	return {
 		sendFile: function(req, res) {
 			var form = new formidable.IncomingForm();
-
 		   	form.parse(req, function(err, fields, files) {
-		   	console.log(err);
-		   	console.log(fields)
-		   	console.log(files)
-			  res.writeHead(200, {'content-type': 'text/plain'});
-			  res.write('received upload:\n\n');
-			  res.end(util.inspect({fields: fields, files: files}));
+		   		
+		   		var workbook = excel.readFile(files.file.path);
+		   		
+		   		for (sheet in workbook.Sheets){
+		   			workbook.Sheets[sheet] = excel.utils.sheet_to_csv(workbook.Sheets[sheet]);
+		   		}
+
+		   		var myExcel = {
+		   			sheets: workbook.SheetNames, 
+		   			content: workbook.Sheets
+		   		}
+
+		   		res.json(myExcel);
+
 			});
 		}
 	}
